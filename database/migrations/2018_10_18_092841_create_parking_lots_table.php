2e70af098a7be7d8d5a9c8b4d08e5c6273fe8a1f
kinahamzah <kinahamzah@gmail.com>
2018-10-18 17:43:47 +0800

    table society and parkingLot

---
+++ database/migrations/2018_10_18_092841_create_parking_lots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParkingLotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parking_lots', function (Blueprint $table) {
+            $table->increments('p_id',10);
+            $table->integer('s_id',10);
+            $table->integer('user_id',10);
+            $table->string('unit_id',10);
+            $table->string('parking_slot',20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parking_lots');
+    }
+}
