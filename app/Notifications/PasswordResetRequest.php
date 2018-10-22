abfe951c9a9ac0fcca618ed09d0ad03052129472
LAPTOP-7VPFGD30\User <haziranr@yahoo.com>
2018-10-22 14:41:55 +0800

    reset password

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -46,7 +46,7 @@ class PasswordResetRequest extends Notification implements ShouldQueue
 
         return (new MailMessage)
                     ->line('You are receiving this email because we received a password reser request for your account.')
-                    ->action('Reset Password', url('$url'))
+                    ->action('Reset Password', url($url))
                     ->line('If you did not request a password reset, no further action is required.');
     }
 
